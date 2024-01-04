b5fb7d8c7e9127d66a795d445695dc8bdf17e968
DevEmad007 <FarshidIbtihaj@gmail.com>
2024-01-04 09:59:05 +0600

    init repo

---
+++ app.js
@@ -0,0 +1,11 @@
+const number = 10;
+
+if (number < 14) {
+    console.log('number is smaller then 14');
+} else if (number < 14) {
+    console.log('number is bigger then 14');
+} else if (number === 14) {
+    console.log("number is equal to 14");
+}
+
+console.log("First Node App !!!");
