d94eadbc3728cbbfeae3874b9caf9bc44771a181
DevEmad007 <FarshidIbtihaj@gmail.com>
2024-01-08 18:50:36 +0600

    bug fixed

--- NODE_JS/13-http.js
+++ NODE_JS/13-http.js
@@ -1,17 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req,res) => {
-    if (req.url === "/") {
+    const url = req.url;
+    if (url === "/") {
         res.end("Welcome to our home page");
     }
-    if (req.url === "/about") {
+    else if (url === "/about") {
         res.end("hey! It's the About Page innit?");
+    } else {
+        res.write(`
+         <h1>OPPS</h1>
+         <p>404 Page Not Found</p>
+         `);
+        res.end();
     }
-    res.write(`
-    <h1>OPPS</h1>
-    <p>404 Page Not Found</p>
-    `);
-    res.end();
 });
 
-server.listen(6969);
+server.listen(5000,() => {
+    console.log("server started");
+});
